b2bb9a06dbcb4429a808ddf3825fd08b17f03456
Dantas Ferreira Crispim <dantas10.crispim@gmail.com>
2024-10-20 17:42:11 -0300

    add project

---
+++ index.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const artistData = [
+        { name: 'Henrique & Juliano', image: './img/artista-henrique-juliano.jpg' },
+        { name: 'Jorge & Mateus', image: './img/artista-jorge-mateus.jpg' },
+        { name: 'Zé Neto & Cristiano', image: './img/artista-ze-neto.jpg' },
+        { name: 'Gustavo Lima', image: './img/artista-gustavo-limma.jpg' },
+        { name: 'Luan Santana', image: './img/artista-luan-santana.jpg' },
+        { name: 'Matheus & Kaun', image: './img/artista-mateus-kauan.jpg' },
+    ];
+    
+    const albumsData = [
+        { name: 'White Noise (Sleep & Relaxation Soounds)', artist: "Sleepy Jonh", image: './img/album-white-noise.jpg' },
+        { name: 'O Céu Explica Tudo (Ao Vivo)', artist: "Henrique & Juliano", image: './img/album-ceu-explica.jpg' },
+        { name: 'Nada como o dia apos o outro', artist: "Racionais", image: './img/album-racionais.jpg' },
+        { name: 'HIT ME HARD AND SOFT', artist: "Billie Eilish", image: './img/album-hit-me.jpg' },
+        { name: 'CAJU', artist: "Liniker", image: './img/album-caju.jpg' },
+        { name: 'Escândalo Íntimo', artist: "Luisa Sonza", image: './img/album-escandalo.jpg' },
+    
+    ];
+
+    const artistGrid = document.querySelector('.artists-grid')
+    const albumsGrid = document.querySelector('.albums-grid')
+
+    artistData.forEach( artist => {
+        const artistCard = document.createElement('div')
+        artistCard.classList.add('artist-card')
+
+        artistCard.innerHTML = ` 
+            
+            <img src="${artist.image}" alt="imagem do ${artist.name}">
+            <div>
+                <h3>${artist.name}</h3>
+                <p>artista</p>
+            </div>
+
+        `
+
+        artistGrid.appendChild(artistCard)
+    })
+
+    albumsData.forEach(album => {
+        const albumCard = document.createElement('div')
+        albumCard.classList.add('album-card')
+
+        albumCard.innerHTML = `
+
+            
+            <img src = "${album.image}" alt = "imagem do ${album.name}" >
+            <div>
+                <h3>${album.name}</h3>
+                <p>${album.artist}</p>
+            </div>
+        `
+
+        albumsGrid.appendChild(albumCard)
+
+    })
+    
+})
+
+
+
+
+
+
+
